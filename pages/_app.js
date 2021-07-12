d5359df9ba142b96db4c6d3b48cc613499bcdb71
the-one-who-knoccks <thiagoalves.devp@gmail.com>
2021-07-12 19:24:51 -0300

    :recycle: Refactored the code

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,30 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  body {
+ /* Reset CSS */
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  body {
+    font-family: sans-serif;
+    background: #D9E6F6;
+  }
+
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
   }
 `
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: "#0070f3",
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +34,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
